eeac970e514f87f1d961e3d9448407724e700200
Aram <aram.miqayelyan@gmail.com>
2019-02-08 20:51:46 +0400

    updated package.json, package-lock.json and added filters.test.js

---
+++ src/tests/actions/filters.test.js
@@ -0,0 +1,6 @@
+import moment from "moment";
+import { setStartDate, setEndDate } from "../../actions/filters";
+
+test("should generate set start date action object", () => {});
+
+test("should generate set end date action object", () => {});
